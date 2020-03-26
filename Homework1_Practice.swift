e4b1384242cb2cbe6b7f81e36b8735fbe8b95651
Jocelyn <59553647+JocelynMilRodriguez@users.noreply.github.com>
2020-03-26 14:03:48 -0400

    Update Homework1_Practice.swift

--- Homework1_Practice.swift
+++ Homework1_Practice.swift
@@ -14,14 +14,29 @@ struct aPatient //Building a patient form for Potential COVID-19 testing
     var Fever: Bool                // Does the patient present a fever?
     var Cough: Bool                // Does the patient present a cough?
     var ShortnessOfBreath: Bool    // Does the patient present shortness of breath?
+    
+    func patientData()
+    {
+        print("Hello. My name is \(NameOfPatient).");
+        print("Do you have a fever? \(Fever).");
+        print("Do you have a cough? \(Cough).");
+        print("Do you have shortness of breath? \(ShortnessOfBreath).");
+        print("                                        ");
+    
+    }
 }
 
-// Construct instances for each Patient
+// Construct data for each Patient
 
 let patient1: aPatient = aPatient(NameOfPatient: "Mary B. Woolsworth", Fever: true, Cough: true, ShortnessOfBreath: false);
 let patient2: aPatient = aPatient(NameOfPatient: "Blake Davis", Fever: false, Cough: true, ShortnessOfBreath: false);
 let patient3: aPatient = aPatient(NameOfPatient: "Melissa Cruz-Santana", Fever: false, Cough: true, ShortnessOfBreath: true);
 let patient4: aPatient = aPatient(NameOfPatient: "Tosan Abiola", Fever: false, Cough: false, ShortnessOfBreath: false);
+
+patient1.patientData();
+patient2.patientData();
+patient3.patientData();
+patient4.patientData();
 
 // Print the patient data
 
@@ -29,5 +44,4 @@ print(patient1);
 print(patient2);
 print(patient3);
 print(patient4);
-
 
