a0e0dea6de1a01c01ef5627485818306f8656368
Jocelyn <59553647+JocelynMilRodriguez@users.noreply.github.com>
2020-03-26 02:13:06 -0400

    Add files via upload

---
+++ Homework1_Practice.swift
@@ -0,0 +1,35 @@
+
+/*
+Jocelyn M. Rodriguez
+March 25th 2020
+Homework#1
+Input: No User Input
+Output: the data for each patient entering the Emergency Room
+Purpose: To collect and display each patients' name and list of symptoms
+*/
+
+import UIKit
+
+struct aPatient //Building a patient form for Potential COVID-19 testing
+{
+    var NameOfPatient: String      // Name of each patient
+    var Fever: Bool                // Does the patient present a fever?
+    var Cough: Bool                // Does the patient present a cough?
+    var ShortnessOfBreath: Bool    // Does the patient present shortness of breath?
+}
+
+// Construct data for each Patient
+
+let patient1: aPatient = aPatient(NameOfPatient: "Mary B. Woolsworth", Fever: true, Cough: true, ShortnessOfBreath: false);
+let patient2: aPatient = aPatient(NameOfPatient: "Blake Davis", Fever: false, Cough: true, ShortnessOfBreath: false);
+let patient3: aPatient = aPatient(NameOfPatient: "Melissa Cruz-Santana", Fever: false, Cough: true, ShortnessOfBreath: true);
+let patient4: aPatient = aPatient(NameOfPatient: "Tosan Abiola", Fever: false, Cough: false, ShortnessOfBreath: false);
+
+// Print the patient data
+
+print(patient1);
+print(patient2);
+print(patient3);
+print(patient4);
+
+
