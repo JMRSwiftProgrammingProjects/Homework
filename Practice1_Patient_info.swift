80c989c1e8f4fb8bdaba740660f9d0b13e56aa8b
Jocelyn <59553647+JocelynMilRodriguez@users.noreply.github.com>
2020-03-27 09:23:59 -0400

    Update Practice1_Patient_info.swift

--- Practice1_Patient_info.swift
+++ Practice1_Patient_info.swift
@@ -19,7 +19,9 @@ struct Patient //Building a patient form for Potential COVID-19 testing
     func patientData()
     {
         //Multiline string literals
-        print("""Hello. My name is \(nameOfPatient). 
+        
+        print("""
+                Hello. My name is \(nameOfPatient). 
                 Do you have a fever? \(fever).
                 Do you have a cough? \(cough).
                 Do you have shortness of breath? \(shortnessOfBreath).""");
